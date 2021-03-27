706ef2f41f09fcafcc6add9bf3ee4ed1e46f7e94
김현석 <@DESKTOP-8G5I1U8>
2021-03-27 17:41:09 +0900

    백준 2439

--- src/for_test/Ex2439.java
+++ src/for_test/Ex2439.java
@@ -1,10 +1,22 @@
 package for_test;
 
+import java.util.Scanner;
+
 public class Ex2439 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i =1 ; i<=n; i++) {
+			for(int k=1; k<=n-i; k++)
+				System.out.printf(" ");
+			for(int j=1; j<=i; j++)
+				System.out.printf("*");
+		
+			System.out.println();
+		}
 	}
 
 }
