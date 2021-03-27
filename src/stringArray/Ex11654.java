6d55db715d9f9b2be326969de97ea0ff27e5314f
김현석 <@DESKTOP-8G5I1U8>
2021-03-27 20:42:18 +0900

    예제 11654

---
+++ src/stringArray/Ex11654.java
@@ -0,0 +1,14 @@
+package stringArray;
+
+import java.util.Scanner;
+
+public class Ex11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int c =(int) sc.nextLine().charAt(0);
+		
+		System.out.printf("%d", c);
+	}
+
+}
